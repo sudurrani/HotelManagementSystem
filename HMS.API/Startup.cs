4ad745c81fbb7ca50cbd24562f8f00b9ce278b5c
Shahid Ullah <shahidullahdurrani@gmail.com>
2022-06-29 19:06:26 +0500

    Add Cors - Allow Anonymouse

--- HMS.API/Startup.cs
+++ HMS.API/Startup.cs
@@ -105,11 +105,15 @@ namespace HMS.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)//,HMSDbContext hmsDbContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,HMSDbContext hmsDbContext)
         {
             // migrate any database changes on startup (includes initial db creation)
-            //hmsDbContext.Database.Migrate();
-
+            hmsDbContext.Database.Migrate();
+            //Added this code into Startup -> Configure method to allow request from any site
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
